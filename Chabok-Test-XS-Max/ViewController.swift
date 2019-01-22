1837820927e133b8afc06c975c7a8561d4c4a40f
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-01-22 11:33:36 +0330

    Initial Commit

---
+++ Chabok-Test-XS-Max/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chabok-Test-XS-Max
+//
+//  Created by Hussein Habibi Juybari on 1/22/19.
+//  Copyright © 2019 Chabok Realtime Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
